96a52b205fd5b78f5e8da6f772a10599e69cabe4
GAETAN-R-BLUE <gaetanrodrigue_98@yahoo.com>
2025-05-23 08:37:58 -0400

    $$

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -8,7 +8,7 @@ public class Arithmetic {
         System.out.println("1. Addition:");
         System.out.println("2. Subtraction:");
         System.out.println("3. Multiplication:");
-        System.out.println("4. Division:");
+        System.out.println("4. Division: ");
 
 
     }
